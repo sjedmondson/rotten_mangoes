f01404758bdb6a37c1cb5fc77f0c059ba0e9a938
Simon Edmondson <simonedmondson@live.ca>
2016-08-18 01:31:00 +0000

    deleting users and their reviews completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
 
-  has_many :reviews
+  has_many :reviews, :dependent => :destroy
 
-  after_destroy :user_delete_notification
+  #after_destroy :user_delete_notification
 
   has_secure_password
   validates :email, presence: true
